8740d2422cd4d050a2e59d0f0b6a514aae092a5e
ilkkarajalin <ilkka.rajalin@hotmail.fi>
2019-09-19 21:23:07 +0300

    index ja css aloitus

---
+++ menu.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Ruokakalenteri</title>
+    <link rel="stylesheet" href="css/mystyle.css">
+  </head>
+  <body>
+    <nav>
+      <ul>
+    <li><a href="#">Ruokakunta</a></li>
+    <li><a href="#">Ruokakalenteri</a></li>
+    <li><a href="#">Ostoslista</a></li>
+    <li><a href="#">Ostokset</a></li>
+    <li><a href="#">Reseptit</a></li>
+    <li><a href="#">Ruoka-aineet</a></li>
+    <li><a href="#">Ylläpito</a></li>
+      </ul>
+  </nav>
