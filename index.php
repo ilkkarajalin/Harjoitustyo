02661a38def0905a9656a7777ddb0b8450531e6e
ilkkarajalin <ilkka.rajalin@hotmail.fi>
2019-09-20 18:39:11 +0300

    index päivitys

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <?php include "menu.php"; ?>
-    <form class="" action="show_post.php" method="post">
-      <input type="text" name="ruokakunta" values="" placeholder="Rajaa ruokakuntia tai nimeä uusi" size=30>
-      <input type="submit" name="lisaa_ruokakunta" value="Lisää uusi">
 
-    <br>
-    Ruokakunnat:<br>
-    <select class="" name="ruokakunnat" size="20">
+    <section_3part>
+    <div_3part>
+
+      <form class="" action="show_post.php" method="post">
+        <input type="text" name="ruokakunta" values="" placeholder="Rajaa tai nimeä uusi" size=15>
+        <input type="submit" name="lisaa_ruokakunta" value="Lisää uusi">
+        </form>
+        <br>
+      Ruokakunnat:<br>
+    <select class="non_scroll" name="ruokakunnat" size="20" width="50">
       <option value="ruokakunnat0">Mainiot</option>
       <option value="ruokakunnat1">Juoniot</option>
       <option value="ruokakunnat2">Meikäläiset</option>
@@ -13,14 +17,24 @@
       <option value="ruokakunnat4">Korhoset</option>
     </select>
 
+  </div_3part>
     <br>
-    Ruokakunnan jäsenet:<br>
-    <select class="" name="jasenet" size="20">
+  <div_3part>
+    <br>
+    <br>
+    Ruokakunnan jäsenet:
+    <br>
+    <select class="non_scroll" name="jasenet" size="20">
       <option value="r0jasen0">Mainiot</option>
       <option value="r0jasen1">Juoniot</option>
       <option value="r0jasen2">Meikäläiset</option>
     </select>
+    </div_3part>
     <br>
+    <div_3part>
+      <br>
+      <br>
+      <br>
     <input type="text" name="nimi_uusi_jasen" value="" placeholder="Uuden jäsenen nimi">
     <br>
     <input type="submit" name="lisaa_jasen" value="<- Lisää jäsen">
@@ -28,8 +42,9 @@
     <br>
     <br>
     <input type="submit" name="lisaa_jasen" value="Poista jäsen ->">
+</div_3part>
 
-</form>
 
+</section_3part>
   </body>
 </html>
