10afc1f0be37a774cd9bdb991081a529f374b2f2
ilkkarajalin <ilkka.rajalin@hotmail.fi>
2019-09-21 11:56:18 +0300

    index2 päivitys ja ruokakalenterin aloitus

---
+++ ruokakalenteri.php
@@ -0,0 +1,53 @@
+<?php include "menu.php"; ?>
+<br>
+Valitse ruokakunta:
+<br>
+<form action="">
+  <select name="ruokakunta" onchange="">
+    <option value="">Mainiot</option>
+    <option value="">Juoniot</option>
+    <option value="">Meikäläiset</option>
+    <option value="">Virtaset</option>
+    <option value="">Korhoset</option>
+  </select>
+</form>
+<br>
+<form action"">
+  <input type="submit" name="vko_alas" value="<<">
+  Vko 39
+  <input type="submit" name="vko_ylos" value=">>">
+  <br>
+  dd.mm.yyyy - dd.mm.yyy
+</from>
+<br>
+<br>
+
+<table id="ruokakalenteri">
+  <tr>
+    <th></th>
+    <th>Ma</th>
+    <th>Ti</th>
+    <th>Ke</th>
+    <th>To</th>
+    <th>Pe</th>
+    <th>La</th>
+    <th>Su</th>
+  </tr>
+  <tr>
+    <th>Aamiainen</th>
+  </tr>
+  <tr>
+    <th>Lounas</th>
+    <td>Lasagne <a href="#">(x)</a></td>
+
+  </tr>
+  <tr>
+    <th>Päivällinen</th>
+  </tr>
+  <tr>
+    <th>Iltapala</th>
+  </tr>
+  <tr>
+    <th>Välipalat</th>
+  </tr>
+</table>
