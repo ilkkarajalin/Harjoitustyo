21065ddd10213ba423f788671e8e3cdbf17edb59
ilkkarajalin <ilkka.rajalin@hotmail.fi>
2019-10-12 13:27:44 +0300

    kirjautuminen tietokannasta, ostoslista tehty

--- login.php
+++ login.php
@@ -1,28 +1,115 @@
 <?php
+include "connection.php";
+
 if(session_status() !== PHP_SESSION_ACTIVE) session_start();
 $salasana=$_POST['salasana'];
 $kayttaja=$_POST['kayttaja'];
-$oikea_salasana='salasana';
 
-if($salasana==$oikea_salasana)
+if(isset($_POST['kirjaudu']))
 {
-  $_SESSION['logged_in']=true;
-  $_SESSION['username']=$kayttaja;
-  $_SESSION['user_id']=1;
-  $_SESSION['ruoka_aine_valittu']=false;
-  include "menu.php";
-  echo '<section_3part><div_3part>';
-  echo '<p>Olet kirjautunut Reseptoriin käyttäjänä '.$kayttaja.'</p>';
-}
-  else
+
+  $stmt=$db->prepare("SELECT salasana FROM kayttajat WHERE kayttaja=:username");
+  $stmt->bindParam(':username',$kayttaja);
+  $stmt->execute();
+
+  $oikea_salasana = $stmt->fetch(PDO::FETCH_COLUMN);
+
+  if(password_verify($salasana,$oikea_salasana))
   {
+    $_SESSION['logged_in']=true;
+    $_SESSION['username']=$kayttaja;
+
+    $stmt=$db->prepare("SELECT kayttaja_id FROM kayttajat WHERE kayttaja=:username");
+    $stmt->bindParam(':username',$kayttaja);
+    $stmt->execute();
+    $kayttaja_id = $stmt->fetch(PDO::FETCH_COLUMN);
+
+    $_SESSION['user_id']=$kayttaja_id;
+    $_SESSION['ruoka_aine_valittu']=false;
     include "menu.php";
     echo '<section_3part><div_3part>';
-    echo '<p>Käyttäjätunnus ja salasana eivät täsmää. Kirjaudu uudelleen tai luo uusi käyttäjä</p>';
-    echo '<br><br>';
-    echo '<a href="index.php">Kirjaudu uudelleen</a>';
+    echo '<p>Olet kirjautunut Reseptoriin käyttäjänä '.$kayttaja.'</p>';
   }
- ?>
+    else
+    {
+      include "menu.php";
+      echo '<section_3part><div_3part>';
+      echo '<p>Käyttäjätunnus ja salasana eivät täsmää. Kirjaudu uudelleen tai luo uusi käyttäjä</p>';
+      echo '<br><br>';
+      echo '<a href="index.php">Kirjaudu uudelleen</a>';
+    }
+  }
+  else
+  {
+      if(isset($_POST['salasana']) AND isset($_POST['kayttaja']))
+      {
+        if ($_POST['salasana'] != '' AND $_POST['kayttaja'] != '')
+        {
+
+        $stmt=$db->prepare("SELECT COUNT(kayttaja_id) FROM kayttajat WHERE kayttaja=:username");
+        $stmt->bindParam(':username',$kayttaja);
+        $stmt->execute();
+        $kysely = $stmt->fetch(PDO::FETCH_COLUMN);
+
+        if($kysely == 0)
+        {
+          $koodattu_salasana = password_hash($_POST['salasana'],PASSWORD_DEFAULT);
+          $stmt=$db->prepare("INSERT INTO kayttajat VALUES(NULL,:username,:password)");
+          $stmt->bindParam(':username',$kayttaja);
+          $stmt->bindParam(':password',$koodattu_salasana);
+          $stmt->execute();
+
+          $_SESSION['logged_in']=true;
+          $_SESSION['username']=$kayttaja;
+
+          $stmt=$db->prepare("SELECT kayttaja_id FROM kayttajat WHERE kayttaja=:username");
+          $stmt->bindParam(':username',$kayttaja);
+          $stmt->execute();
+          $kysely = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+          foreach ($kysely as $row)
+          {
+            $kayttaja_id = $row['kayttaja_id'];
+          }
+
+          //echo 'Kayttaja_id='.$kayttaja_id;
+
+          $_SESSION['user_id']=$kayttaja_id;
+          $_SESSION['ruoka_aine_valittu']=false;
+          include "menu.php";
+          echo '<section_3part><div_3part>';
+          echo '<p>Olet kirjautunut Reseptoriin käyttäjänä '.$kayttaja.'</p>';
+        }
+        else
+        {
+          include "menu.php";
+          echo '<section_3part><div_3part>';
+          echo 'Käyttäjätunnus on jo käytössä. Anna uusi käyttäjätunnus';
+          echo '<br><br>';
+          echo '<a href="index.php">Kirjaudu uudelleen</a>';
+        }
+        }
+        else
+        {
+          include "menu.php";
+          echo '<section_3part><div_3part>';
+          echo 'Käyttäjätunnus ja salasana tulee antaa uuden käyttäjän luonnissa';
+          echo '<br><br>';
+          echo '<a href="index.php">Kirjaudu uudelleen</a>';
+        }
+      }
+      else
+      {
+        include "menu.php";
+        echo '<section_3part><div_3part>';
+        echo 'Käyttäjätunnus ja salasana tulee antaa uuden käyttäjän luonnissa';
+        echo '<br><br>';
+        echo '<a href="index.php">Kirjaudu uudelleen</a>';
+      }
+
+
+  }
+   ?>
 </div_3part>
 </section_3part>
 
--- ostoslista.php
+++ ostoslista.php
@@ -2,7 +2,14 @@
 <?php include "connection.php"; ?>
 
 <?php
+print_r($_POST);
+
 $mod_today = 0;
+
+if(isset($_POST['ruokakunta']))
+{
+  $_SESSION['ostoslista_ruokakunta_id'] = $_POST['ruokakunta'];
+}
 
 if(!isset($_SESSION['mod_week_ostos']))
 {
@@ -98,7 +105,7 @@ $n_ruokailut = array (0,0,0,0,0);
 for($q=0;$q<5;$q++)
 {
   $query_str = "SELECT count(id) FROM ruokakalenteri WHERE ruokailu='".$ruokailut[$q]."' AND pvm>='".$query_start."' AND pvm <='".$query_end."'";
-  echo '<br>'.$query_str.'<br>';
+  //echo '<br>'.$query_str.'<br>';
   $kysely=$db->query($query_str);
 
   foreach ($kysely as $row)
@@ -123,8 +130,17 @@ for($q=0;$q<5;$q++)
         {
           echo '<b>'.$ruokailut_nimi[$x].' '.$n_ruokailut[$x].' kpl</b><br>';
 
-          $query_str = "SELECT count(id) FROM ruokakalenteri WHERE ruokailu='".$ruokailut[$q]."' AND pvm>='".$query_start."' AND pvm <='".$query_end."'";
-          echo '<br>'.$query_str.'<br>';
+          $query_str = "SELECT ruokakalenteri.ruokailu,reseptit.resepti,COUNT(ruokakalenteri.resepti_id) AS lkm FROM ruokakalenteri JOIN reseptit ON reseptit.resepti_id=ruokakalenteri.resepti_id WHERE ruokakalenteri.ruokakunta_id=".$_SESSION['ostoslista_ruokakunta_id']." AND ruokakalenteri.pvm>='".$query_start."' AND ruokakalenteri.pvm<='".$query_end."' AND ruokakalenteri.ruokailu='".$ruokailut[$x]."' GROUP BY ruokakalenteri.resepti_id";
+          //echo '<br>'.$query_str.'<br>';
+          $kysely=$db->query($query_str);
+
+          foreach($kysely as $row)
+          {
+            $resepti_nimi = $row['resepti'];
+            $lkm = $row['lkm'];
+            echo '<tab4>'.$resepti_nimi.' '.$lkm.' kpl<br>';
+          }
+
 
         }
 
@@ -139,7 +155,6 @@ for($q=0;$q<5;$q++)
     <?php
     $ainekset = array();
 
-
     ?>
 
 
@@ -149,20 +164,46 @@ for($q=0;$q<5;$q++)
       <th>Yksikkö</th>
       <th>Hinta á</th>
       <th>Yhteensä</th>
+      <th>Paino kg</th>
       <tr>
-        <td>Kaurahiutaleet</td>
-        <td>1</td>
-        <td>Pkt</td>
-        <td>2.59 €</td>
-        <td>2.59 €</td>
-      <tr>
-        <td>Kaurahiutaleet</td>
-        <td>1</td>
-        <td>Pkt</td>
-        <td>2.59 €</td>
-        <td>2.59 €</td>
 
+        <?php
+        $query_str = "select ruoka_aineet.ruoka_aine,reseptin_aineet.kaytto_maara*count(ruokakalenteri.resepti_id) AS lkm,AVG(ostokset.hinta_hankinta_yks) AS hinta,ruoka_aineet.paino_kaytto_yks,ruoka_aineet.paino_hankinta_yks,yksikot.yksikko FROM ruoka_aineet JOIN reseptin_aineet ON reseptin_aineet.ruoka_aine_id=ruoka_aineet.ruoka_aine_id JOIN reseptit ON reseptit.resepti_id=reseptin_aineet.resepti_id JOIN ruokakalenteri ON ruokakalenteri.resepti_id=reseptit.resepti_id JOIN ostokset ON ostokset.ruoka_aine_id=ruoka_aineet.ruoka_aine_id JOIN yksikot ON yksikot.yksikko_id=ruoka_aineet.hankinta_yks_id WHERE ruokakalenteri.ruokakunta_id=".$_SESSION['ostoslista_ruokakunta_id']." AND ruokakalenteri.pvm>='".$query_start."' AND ruokakalenteri.pvm<='".$query_end."' GROUP BY ruoka_aineet.ruoka_aine_id";
 
+        //echo '<br>'.$query_str.'<br>';
+
+        $kysely=$db->query($query_str);
+        $yhteensa = 0;
+        $paino_yht = 0;
+
+        foreach($kysely as $row)
+        {
+          $ruoka_aine_nimi = $row['ruoka_aine'];
+          $lkm = $row['lkm'];
+          $hinta = round($row['hinta'],2);
+          $paino_kaytto = $row['paino_kaytto_yks'];
+          $paino_hankinta = $row['paino_hankinta_yks'];
+          $yksikko = $row['yksikko'];
+
+          $lkm = round($lkm * $paino_kaytto / $paino_hankinta,2);
+
+          $paino = round($lkm * $paino_hankinta/1000,2);
+
+          echo '<td>'.$ruoka_aine_nimi.'</td>';
+          echo '<td>'.$lkm.'</td>';
+          echo '<td>'.$yksikko.'</td>';
+          echo '<td>'.$hinta.' €</td>';
+          echo '<td>'.round($hinta * $lkm,2).' €</td>';
+          echo '<td>'.$paino.'</td><tr>';
+
+          $yhteensa = $yhteensa + round($hinta * $lkm,2);
+          $paino_yht = $paino_yht + $paino;
+
+        }
+
+        echo '<th>Yhteensä</th><th></th><th></th><th></th><th>'.$yhteensa.' €</th><th>'.$paino_yht.'<tr>';
+
+        ?>
     </table>
   </div>
   </div_3part>
